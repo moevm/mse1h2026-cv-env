4adfecab723a664a1488dfc2195a00ca67528250
alexanderromanoff <worktimepostbox2019@gmail.com>
2026-03-17 15:03:12 +0300

    create useObjectUrl hook

---
+++ frontend/src/hooks/useObjectUrl.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export default function useObjectUrl(file) {
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    if (!file) {
+      setUrl(null);
+      return;
+    }
+    const objectUrl = URL.createObjectURL(file);
+    setUrl(objectUrl);
+    return () => {
+      URL.revokeObjectURL(objectUrl);
+    };
+  }, [file]);
+
+  return url;
+}
